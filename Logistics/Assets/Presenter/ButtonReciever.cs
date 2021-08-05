f4416ad66bfa66c3c850915cd38bf7df7e45e136
Will <will.tripp@btinternet.com>
2021-08-05 10:04:38 +0100

    Changed name of MVC folder to Presenter

---
+++ Logistics/Assets/Presenter/ButtonReciever.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BUTTON_FUNCTION { OPEN_TRANSPORT_MENU, OPEN_VEHICLE_MENU,CANCEL,CONFIRM, TOGGLE_PAUSE_PLAY, DOUBLE_SPEED }
+
+public static class ButtonReciever {
+
+    private static Dictionary<BUTTON_FUNCTION, UI_STATE> stateChangingUIDictionary = new Dictionary<BUTTON_FUNCTION, UI_STATE>()
+    {
+        { BUTTON_FUNCTION.OPEN_TRANSPORT_MENU,UI_STATE.TRANSPORT_MENU },
+        { BUTTON_FUNCTION.OPEN_VEHICLE_MENU,UI_STATE.VEHICLE_MENU }
+    };
+
+	public static void recieveButtonClick(BUTTON_FUNCTION function)
+    {
+        switch (function)
+        {
+            case BUTTON_FUNCTION.CONFIRM:
+                ConfirmationManager.confirm();
+                UIStateManager.changeState(UI_STATE.NEUTRAL);
+                break;
+            case BUTTON_FUNCTION.CANCEL:
+                ConfirmationManager.cancel();
+                WorldUIStateManager.clear();
+                break;
+            case BUTTON_FUNCTION.TOGGLE_PAUSE_PLAY:
+                TimeManager.togglePausePlay();
+                break;
+            case BUTTON_FUNCTION.DOUBLE_SPEED:
+                TimeManager.toggleDoubleSpeed();
+                break;
+            default:
+                UI_STATE nextState;
+                if (stateChangingUIDictionary.TryGetValue(function, out nextState)) UIStateManager.changeState(nextState);
+                else throw new System.Exception("Trying to use state changing dictionary for incompatible button");
+                break;
+        }
+    }
+
+    public static void recieveButtonClick(VEHICLE vehicle)
+    {
+        UIStateManager.changeState(UI_STATE.BLANK);
+        VehicleCreatorManager.startCreation(VehicleCreatorFactory.build(vehicle));
+    }
+
+    public static void recieveButtonClick(FEATURES feature)
+    {
+        UIStateManager.changeState(UI_STATE.BLANK);
+        DrawingManager.startDrawing(DrawerFactory.build(feature));
+    }
+}
---
+++ Logistics/Assets/Presenter/UIStateManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UI_STATE { BLANK, NEUTRAL, VEHICLE_MENU,TRANSPORT_MENU,MENU}
+
+public static class UIStateManager {
+
+    private static List<GameObject> currentUI = new List<GameObject>();
+    private static UI_STATE _state = UI_STATE.BLANK; //public static UI_STATE state { get { return _state; } }
+    private static UI_STATE prevState = UI_STATE.BLANK;
+
+    private static readonly Dictionary<UI_STATE, UI_STATE> parentStateOf = new Dictionary<UI_STATE, UI_STATE>()
+    {
+        {UI_STATE.NEUTRAL,UI_STATE.MENU },
+        {UI_STATE.VEHICLE_MENU,UI_STATE.NEUTRAL },
+        {UI_STATE.TRANSPORT_MENU,UI_STATE.NEUTRAL },
+        {UI_STATE.MENU,UI_STATE.NEUTRAL }
+    };
+
+    private static Dictionary<UI_STATE, GameObject[]> objectsToRenderDictionary = new Dictionary<UI_STATE, GameObject[]>()
+    {
+        { UI_STATE.NEUTRAL, new GameObject[] {Assets.assets.openTransportButtonPrefab,Assets.assets.openVehicleButtonPrefab } },
+        { UI_STATE.BLANK, new GameObject[0] },
+        { UI_STATE.VEHICLE_MENU, new GameObject[] {Assets.assets.vehicleBarPrefab } },
+        { UI_STATE.TRANSPORT_MENU, new GameObject[] {Assets.assets.transportBarPrefab } }
+    };
+
+	public static void changeState(UI_STATE state)
+    {
+        destroyCurrent();
+        prevState = _state;
+        _state = state;
+        GameObject[] toCreate;
+        if (objectsToRenderDictionary.TryGetValue(_state, out toCreate)) addAndRenderObjects(toCreate);
+        else throw new System.Exception("State not implemented in dictionary "+state);
+    }
+
+    public static void exitState()
+    {
+        UI_STATE parentState;
+        if(parentStateOf.TryGetValue(_state,out parentState))
+        {
+            changeState(parentState);
+        }
+        else
+        {
+            changeState(prevState);
+        }
+    }
+
+    private static void addAndRenderObjects(GameObject[] toCreate)
+    {
+        for (int i = 0; i < toCreate.Length; i++)
+        {
+            currentUI.Add(Object.Instantiate(toCreate[i], Assets.assets.canvas));
+        }
+    }
+
+    private static void destroyCurrent()
+    {
+        for(int i = currentUI.Count - 1;i >= 0; i--)
+        {
+            Object.Destroy(currentUI[i]);
+            currentUI.RemoveAt(i);
+        }
+        WorldUIStateManager.clear();
+    }
+}
